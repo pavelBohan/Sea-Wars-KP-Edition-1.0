2410797e8c0cde0407891177bcfa57ec63291923
Pavel B. <pavelbohan9@gmail.com>
2024-07-14 12:00:29 +0300

    Добавьте файлы проекта.

--- Sea Wars KP Edition/Sea Wars KP Edition.cpp
+++ Sea Wars KP Edition/Sea Wars KP Edition.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 
+void Rasstonovka();
+void FirstHod();
+void SecondHod();
+
 int main()
 {
     cout << "Hello World!\n";
+    Rasstonovka();
+}
+
+void Rasstonovka() {
+    cout << "тут мы как-то должны расставить корабли на поле" << '\n';
+}
+
+void FirstHod() {
+    cout << "тут первый игрок выбирает ячейку на поле и в массив заносится значение +1" << endl;
+}
+
+void SecondHod() {
+    cout << "тут первый игрок выбирает ячейку на поле и в массив заносится значение +1" << endl;
 }
